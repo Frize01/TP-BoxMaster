c6e9f412dee14fda36122a2607264dd6d9663a2c
Kévin <voisine.kevin2@gmail.com>
2024-10-04 12:19:08 +0200

    [✨ feat] Migration init BDD #10

---
+++ database/migrations/2024_10_04_101258_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('box_id');
+            $table->bigInteger('tenant_id');
+            $table->foreign('tenant_id')->references('id')->on('tenants');
+            $table->float('price');
+            $table->timestamp('date_start');
+            $table->timestamp('date_end');
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
