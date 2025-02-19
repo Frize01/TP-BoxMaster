b408a486f45412a6847c99f96e22bdb215cf8976
Frize <voisine.kevin2@gmail.com>
2025-02-19 01:04:14 +0100

    🎨 front

--- resources/views/impots.blade.php
+++ resources/views/impots.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Accueil
+            Impôts
         </h2>
     </x-slot>
 
@@ -23,19 +23,33 @@
                             <div class="mt-6">
                                 <h4 class="text-xl">Régime Micro-Foncier</h4>
                                 <p class="mt-2">Montant total à déclarer (case 4 BE déclaration n°2042) :
-                                    <span>{{ number_format($totalRevenus, 2) }} €</span></p>
+                                    <span>{{ number_format($totalRevenus, 2) }} €</span>
+                                </p>
                                 <p class="mt-2">Montant imposable (après abattement de 30%) :
-                                    <span>{{ number_format($totalRevenus * 0.7, 2) }} €</span></p>
+                                    <span>{{ number_format($totalRevenus * 0.7, 2) }} €</span>
+                                </p>
                             </div>
                         @else
                             <div class="mt-6">
                                 <h4 class="text-xl">Régime Réel</h4>
                                 <p class="mt-2">Montant total à déclarer (case 4 BA déclaration n°2044) :
-                                    <span>{{ number_format($totalRevenus, 2) }} €</span></p>
+                                    <span>{{ number_format($totalRevenus, 2) }} €</span>
+                                </p>
                                 <p class="mt-2">Montant imposable (100% des revenus) :
-                                    <span>{{ number_format($totalRevenus, 2) }} €</span></p>
+                                    <span>{{ number_format($totalRevenus, 2) }} €</span>
+                                </p>
                             </div>
                         @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+        @forelse
+        <div class="py-6">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <p class="text-gray-500 text-center">Aucun revenu trouvé pour cette année</p>
                     </div>
                 </div>
             </div>
