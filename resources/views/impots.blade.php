31a39ae44b4ad1840eb96bd9796666b004bda6e0
Frize <voisine.kevin2@gmail.com>
2025-02-19 01:06:36 +0100

    📚 wording

--- resources/views/impots.blade.php
+++ resources/views/impots.blade.php
@@ -49,7 +49,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <p class="text-gray-500 text-center">Aucun revenu trouvé pour cette année</p>
+                        <p class="text-gray-500 text-center">Aucun revenu trouvé</p>
                     </div>
                 </div>
             </div>
